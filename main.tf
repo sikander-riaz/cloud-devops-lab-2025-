d781027579787a8eece3f73b64ce6b40cc4e911d
Siku788 <kahmoshlarka@gmail.com>
2025-08-25 20:32:50 +0500

    fixed main.tf

--- main.tf
+++ main.tf
@@ -4,35 +4,42 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
     Name = "Project VPC"
   }
 }
+
 resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"  # Change as needed
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "public_subnet"
   }
 }
 
-
 resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-2a"  # Change as needed
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1a"
   tags = {
     Name = "private_subnet"
   }
 }
 
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "IGW"
+  }
+}
 
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.my_igw.id
@@ -42,26 +49,29 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-
-
 resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "private_route_table"
+  }
 }
 
-
-
-resource "my_vpc" "nat_eip" {
-  vpc = true
+resource "aws_eip" "nat_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.my_igw]
+  tags = {
+    Name = "NAT EIP"
+  }
 }
 
 resource "aws_nat_gateway" "my_nat_gateway" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public_subnet.id
+  depends_on    = [aws_internet_gateway.my_igw]
   tags = {
     Name = "my_nat_gateway"
   }
 }
-
 
 resource "aws_route" "private_route" {
   route_table_id         = aws_route_table.private_route_table.id
@@ -69,65 +79,123 @@ resource "aws_route" "private_route" {
   nat_gateway_id         = aws_nat_gateway.my_nat_gateway.id
 }
 
-
-
 resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
-
-
 
 resource "aws_route_table_association" "private_subnet_association" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_route_table.id
 }
 
-
-resource "aws_security_group" "private_sg" {
-  vpc_id = aws_vpc.my_vpc.id
-  tags = {
-    Name = "private_sg"
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = []
-  }
-}
-
-
-
 resource "aws_security_group" "public_sg" {
-  vpc_id = aws_vpc.my_vpc.id
+  name_prefix = "public-sg-"
+  vpc_id      = aws_vpc.main.id
   tags = {
     Name = "public_sg"
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  name_prefix = "private-sg-"
+  vpc_id      = aws_vpc.main.id
+  tags = {
+    Name = "private_sg"
+  }
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_sg.id]
+  }
+}
+
+variable "AmiLinux" {
+type = map(string)
+  default = {
+    ap-south-1="ami-02d26659fd82cf299"
+  }
 }
 
 
 
+
+variable "region" {
+  default = "ap-south-1"
+}
+
+
+variable "key_name" {
+  default = "sik"
+  description = "the ssh key to use in the EC2 machines"
+}
+resource "aws_instance" "web" {
+  ami           = "${lookup(var.AmiLinux, var.region)}"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.public_sg.id]
+  associate_public_ip_address = true
+  key_name                    = "${var.key_name}"
+
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 20
+  }
+
+  tags = {
+    Name = "BastionHost"
+  }
+}
+
+
+resource "aws_instance" "application" {
+  ami                    =  "${lookup(var.AmiLinux, var.region)}"
+  instance_type          = "t2.small"
+  subnet_id              = aws_subnet.private_subnet.id
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  associate_public_ip_address = false
+  key_name               = "${var.key_name}"
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 20
+  }
+
+  tags = {
+    Name = "AppServer"
+  }
+}
+
+output "application_private_ip" {
+  value = aws_instance.application.private_ip
+}
+
+
 output "vpc_id" {
-  value = aws_vpc.my_vpc.id
+  value = aws_vpc.main.id
 }
 
 output "public_subnet_id" {
@@ -142,52 +210,10 @@ output "nat_gateway_id" {
   value = aws_nat_gateway.my_nat_gateway.id
 }
 
-# Create an EC2 instance
-resource "aws_instance" "bastion" {
-  ami           = "bastion" # Amazon Linux 2 AMI
-  instance_type = "t2.micro"              # Adjust instance type as needed
+# output "bastion_public_ip" {
+#   value = aws_instance.bastion.public_ip
+# }
 
-  subnet_id                   = aws_subnet.public-subnet.id
-  vpc_security_group_ids      = [aws_security_group.example_sg.id]
-  associate_public_ip_address = true
-
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 20 # Adjust volume size as needed
-  }
-
-  key_name = "keys.pem" # Replace with your existing key pair name or create a new one
-
-  tags = {
-    Name = "ExampleInstance"
-  }
-}
-
-
-
-
-# Create an EC2 instance
-resource "aws_instance" "application" {
-  ami           = "ubuntu" # Amazon Linux 2 AMI
-  instance_type = "t2.micro"              # Adjust instance type as needed
-
-  subnet_id                   = aws_subnet.private-subnet.id
-  vpc_security_group_ids      = [aws_security_group.example_sg.id]
-  associate_public_ip_address = true
-
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 50 # Adjust volume size as needed
-  }
-
-  key_name = "keys" # Replace with your existing key pair name or create a new one
-
-  tags = {
-    Name = "ExampleInstance"
-  }
-}
-
-
-
-
-
+# output "application_private_ip" {
+#   value = aws_instance.application.private_ip
+# }
